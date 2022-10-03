d936a9dd9e44b12fa387a50d38e4ea0fe972914b
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2022-10-03 17:35:39 +1300

    Initial commit

---
+++ src/main/java/net/wntiv/inventorymanager/InventoryManager.java
@@ -0,0 +1,10 @@
+package net.wntiv.inventorymanager;
+
+import net.fabricmc.api.ModInitializer;
+
+public class InventoryManager implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/wntiv/inventorymanager/client/InventoryManagerClient.java
@@ -0,0 +1,13 @@
+package net.wntiv.inventorymanager.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class InventoryManagerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
